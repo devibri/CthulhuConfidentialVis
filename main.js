6c86ead73c0c746bcf048ee4c5b067b42fd39e95
Devi Acharya <dacharya64@gmail.com>
2021-10-28 20:23:36 -0700

    changing dashes for yaml file and json file

--- main.js
+++ main.js
@@ -1,8 +1,57 @@
-var url = "https://www.devi-a.com/CthulhuConfidentialVis/scenes/scene_dame.json";
+mermaid.initialize({
+  startOnLoad: true,
+  securityLevel: 'loose'
+});
+
+var loadScene = function(scene_name) {
+  console.log(scene_name);
+    var url = "https://www.devi-a.com/CthulhuConfidentialVis/scenes/" + scene_name.toLowerCase() + ".json";
+
+/* this tells the page to wait until jQuery has loaded, so you can use the Ajax call */
+console.log(url);
+$(document).ready(function(){
+  $.ajax({
+    url: url,
+    dataType: 'json',
+      error: function(){
+        console.log('JSON FAILED for data');
+      },
+    success:function(results){
+  /* the results is your json, you can reference the elements directly by using it here, without creating any additional variables */
+  
+      var sceneInfo = document.getElementById("sceneInfo");
+      // print out the title scene and type
+      sceneInfo.insertAdjacentHTML( 'beforeend', "<h1>" + results.title + " </h1>");
+      sceneInfo.insertAdjacentHTML( 'beforeend', "<p><em>Scene Type: " + results.scene_type + " </em></p>");
+      // print out and format a list of the lead outs
+      sceneInfo.insertAdjacentHTML( 'beforeend', "Lead-Outs: ");
+      results.lead_outs.forEach(function(element) {
+        sceneInfo.insertAdjacentHTML( 'beforeend', element + " | ");
+      });
+      sceneInfo.insertAdjacentHTML( 'beforeend',"<hr>");
+      // print out list of text in the scene
+      results.text.forEach(function(element) {
+        if (element.clue !== undefined) {
+          // print out each individual clue and format with checkbox
+          // check if checkbox should be checked or not 
+          if (element.clue[1].known == true) {
+            sceneInfo.insertAdjacentHTML( 'beforeend', "<p class='hangingindent'><input type='checkbox' checked>  " + element.clue[0] + "</p>");
+          } else {
+             sceneInfo.insertAdjacentHTML( 'beforeend', "<p class='hangingindent'><input type='checkbox'> " + element.clue[0] + "</p>");
+          }
+        } else {
+          sceneInfo.insertAdjacentHTML( 'beforeend', "<p>" + element + " </p>");
+        }
+      }); 
+    } 
+   }) 
+ }) 
+}
 
 /* this tells the page to wait until jQuery has loaded, so you can use the Ajax call */
 
 $(document).ready(function(){
+  var url = "https://www.devi-a.com/CthulhuConfidentialVis/scenes/scene_dame.json";
   $.ajax({
     url: url,
     dataType: 'json',
@@ -43,45 +92,46 @@ $(document).ready(function(){
  }) 
 
 function loadScene(scene_name) {
-  var url = "https://www.devi-a.com/CthulhuConfidentialVis/scenes/" + scene_name + ".json";
+  console.log("clicked");
+//   var url = "https://www.devi-a.com/CthulhuConfidentialVis/scenes/" + scene_name + ".json";
 
-/* this tells the page to wait until jQuery has loaded, so you can use the Ajax call */
-
-$(document).ready(function(){
-  $.ajax({
-    url: url,
-    dataType: 'json',
-      error: function(){
-        console.log('JSON FAILED for data');
-      },
-    success:function(results){
-  /* the results is your json, you can reference the elements directly by using it here, without creating any additional variables */
+// /* this tells the page to wait until jQuery has loaded, so you can use the Ajax call */
+// console.log(url);
+// $(document).ready(function(){
+//   $.ajax({
+//     url: url,
+//     dataType: 'json',
+//       error: function(){
+//         console.log('JSON FAILED for data');
+//       },
+//     success:function(results){
+//   /* the results is your json, you can reference the elements directly by using it here, without creating any additional variables */
   
-      var sceneInfo = document.getElementById("sceneInfo");
-      // print out the title scene and type
-      sceneInfo.insertAdjacentHTML( 'beforeend', "<h1>" + results.title + " </h1>");
-      sceneInfo.insertAdjacentHTML( 'beforeend', "<p><em>Scene Type: " + results.scene_type + " </em></p>");
-      // print out and format a list of the lead outs
-      sceneInfo.insertAdjacentHTML( 'beforeend', "Lead-Outs: ");
-      results.lead_outs.forEach(function(element) {
-        sceneInfo.insertAdjacentHTML( 'beforeend', element + " | ");
-      });
-      sceneInfo.insertAdjacentHTML( 'beforeend',"<hr>");
-      // print out list of text in the scene
-      results.text.forEach(function(element) {
-        if (element.clue !== undefined) {
-          // print out each individual clue and format with checkbox
-          // check if checkbox should be checked or not 
-          if (element.clue[1].known == true) {
-            sceneInfo.insertAdjacentHTML( 'beforeend', "<p class='hangingindent'><input type='checkbox' checked>  " + element.clue[0] + "</p>");
-          } else {
-             sceneInfo.insertAdjacentHTML( 'beforeend', "<p class='hangingindent'><input type='checkbox'> " + element.clue[0] + "</p>");
-          }
-        } else {
-          sceneInfo.insertAdjacentHTML( 'beforeend', "<p>" + element + " </p>");
-        }
-      }); 
-    } 
-   }) 
- }) 
+//       var sceneInfo = document.getElementById("sceneInfo");
+//       // print out the title scene and type
+//       sceneInfo.insertAdjacentHTML( 'beforeend', "<h1>" + results.title + " </h1>");
+//       sceneInfo.insertAdjacentHTML( 'beforeend', "<p><em>Scene Type: " + results.scene_type + " </em></p>");
+//       // print out and format a list of the lead outs
+//       sceneInfo.insertAdjacentHTML( 'beforeend', "Lead-Outs: ");
+//       results.lead_outs.forEach(function(element) {
+//         sceneInfo.insertAdjacentHTML( 'beforeend', element + " | ");
+//       });
+//       sceneInfo.insertAdjacentHTML( 'beforeend',"<hr>");
+//       // print out list of text in the scene
+//       results.text.forEach(function(element) {
+//         if (element.clue !== undefined) {
+//           // print out each individual clue and format with checkbox
+//           // check if checkbox should be checked or not 
+//           if (element.clue[1].known == true) {
+//             sceneInfo.insertAdjacentHTML( 'beforeend', "<p class='hangingindent'><input type='checkbox' checked>  " + element.clue[0] + "</p>");
+//           } else {
+//              sceneInfo.insertAdjacentHTML( 'beforeend', "<p class='hangingindent'><input type='checkbox'> " + element.clue[0] + "</p>");
+//           }
+//         } else {
+//           sceneInfo.insertAdjacentHTML( 'beforeend', "<p>" + element + " </p>");
+//         }
+//       }); 
+//     } 
+//    }) 
+//  }) 
 }
