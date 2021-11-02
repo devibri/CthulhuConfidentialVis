80bd64f5d000a860dc9ebfa4b6f789f75dd3ac7a
Devi Acharya <dacharya64@gmail.com>
2021-11-02 14:10:14 -0700

    clicking a lead now updates flow chart

--- main.js
+++ main.js
@@ -94,6 +94,23 @@ $(document).on("click", "input[name='clue']", function () {
     if (element.clue !== undefined && clueText == element.clue[0]) {
       element.clue[1].known = checked;
     } 
+    // if you click on a lead to another area, update the flowchart to indicate this
+    if (element.clue !== undefined && clueText == element.clue[0] && element.clue[2] !== undefined) {
+      if (checked) {
+        var tag = currentSceneName + " --> " + element.clue[2].goes_to;
+        graphJSON.graph.forEach(function(element, index) {
+          if (element == tag) { // if checked, do a check to add that element, otherwise do a check to remove that element
+            graphJSON.graph.splice(index, 1);
+          } 
+        }); 
+        var newTag = currentSceneName + " ==> " + element.clue[2].goes_to;
+        graphJSON.graph.push(newTag);
+        // otherwise go through and remove the indication that the scene has been completed
+      } 
+      //save the current graph data then redisplay graph
+      localStorage.setItem("graphData", JSON.stringify(graphJSON));
+      loadGraph();
+    }
   }); 
   // after this is done, should update the JSON file
   localStorage.setItem(currentSceneName, JSON.stringify(sceneJSON));
