207128043856390eade20de7db89d383f4f74e20
Devi Acharya <dacharya64@gmail.com>
2021-11-10 14:30:35 -0800

    adding new json

--- character.js
+++ character.js
@@ -6,5 +6,34 @@ window.onload = function () {
 }
 
 function loadProblems() {
-	
+	var url = "https://www.devi-a.com/CthulhuConfidentialVis/data/problems.json";
+    // use AJAX to fetch the appropriate JSON data
+    $.ajax({
+      url: url,
+      dataType: 'json',
+      error: function(){
+        console.log('JSON FAILED for data');
+      },
+      success:function(results){
+        parseProblems(results);
+      } 
+    }) 
+}
+
+function parseProblems(results) {
+	var problemsList1 = document.getElementById("problems1");
+	var problemsList2 = document.getElementById("problems2");
+  	// clear the current thing in the div
+	$('#problems1').empty();
+	$('#problems2').empty();
+	// iterate through the JSON list of problems and display them
+	for (var i = 0; i < results.length; i++) {
+		if (results[i].obtained = false) {
+			if (i % 2 == 0) {
+				problemsList1.insertAdjacentHTML( 'beforeend', "<div class='card'><h1>" + results[i].id + " </h1><p>" + results[i].name + " (<em>" + results[i].type + "</em>)</p><p>" + results[i].description + "</p></div>");
+			} else {
+				problemsList2.insertAdjacentHTML( 'beforeend', "<div class='card'><h1>" + results[i].id + " </h1><p>" + results[i].name + " (<em>" + results[i].type + "</em>)</p><p>" + results[i].description + "</p></div>");
+			}
+		}
+	}
 }
