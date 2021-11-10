92502a20e501bc0af3c5d73f0b7a16d31fbfeae5
Devi Acharya <dacharya64@gmail.com>
2021-11-10 14:13:10 -0800

    adding problems.json

--- character.js
+++ character.js
@@ -1,1 +1,10 @@
 dragula([document.querySelector('#list3'), document.querySelector('#list'), document.querySelector('#list2')]);
+
+
+window.onload = function () {
+  loadProblems();
+}
+
+function loadProblems() {
+	
+}
