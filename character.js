7adddde9685d91ef1f084fd64c708b39138cfe68
Devi Acharya <dacharya64@gmail.com>
2021-11-11 10:10:52 -0800

    uploading edges json

--- character.js
+++ character.js
@@ -2,8 +2,8 @@ dragula([document.querySelector('#list3'), document.querySelector('#list'), docu
 
 
 window.onload = function () {
-	localStorage.clear();
-  loadProblems();
+	//localStorage.clear();
+	loadProblems();
 }
 
 function loadProblems() {
@@ -22,22 +22,47 @@ function loadProblems() {
 }
 
 function parseProblems(results) {
-	var problemsList1 = document.getElementById("problems1");
-	var problemsList2 = document.getElementById("problems2");
+	var problemsList = document.getElementById("list");
   	// clear the current thing in the div
-	$('#problems1').empty();
-	$('#problems2').empty();
+	$('#list').empty();
 	// iterate through the JSON list of problems and display them
 	for (var i = 0; i < results.length; i++) {
-		console.log(results[i])
-		// if (results[i].obtained == false) {
-			console.log(results[i].obtained);
-			if (i % 2 == 0) {
-				console.log(results[i].obtained);
-				problemsList1.insertAdjacentHTML( 'beforeend', "<div class='card'><h1>" + results[i].id + " </h1><p>" + results[i].name + " (<em>" + results[i].type + "</em>)</p><p>" + results[i].description + results[i].obtained + "</p></div>");
-			} else {
-				problemsList2.insertAdjacentHTML( 'beforeend', "<div class='card'><h1>" + results[i].id + " </h1><p>" + results[i].name + " (<em>" + results[i].type + "</em>)</p><p>" + results[i].description + "</p></div>");
-			}
+		if (results[i].obtained == false) {
+			var html = generateString(results[i]);
+			problemsList.insertAdjacentHTML( 'beforeend', html);
 		}
-	// }
+	}
+}
+
+function loadEdges() {
+	var url = "https://www.devi-a.com/CthulhuConfidentialVis/data/edges.json";
+    // use AJAX to fetch the appropriate JSON data
+    $.ajax({
+      url: url,
+      dataType: 'json',
+      error: function(){
+        console.log('JSON FAILED for data');
+      },
+      success:function(results){
+        parseEdges(results);
+      } 
+    }) 
+}
+
+function parseEdges(results) {
+	var edgesList = document.getElementById("list2");
+  	// clear the current thing in the div
+	$('#list2').empty();
+	// iterate through the JSON list of problems and display them
+	for (var i = 0; i < results.length; i++) {
+		if (results[i].obtained == false) {
+			var html = generateString(results[i]);
+			edgesList.insertAdjacentHTML( 'beforeend', html);
+		}
+	}
+}
+
+function generateString(result) {
+	var string = "<div class='card'><h1>" + result.id + " </h1><p>" + result.name + " " + result.type + "</p><p>" + result.description + "</p></div>"
+	return string;
 }
