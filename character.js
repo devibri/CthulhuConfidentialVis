03e00893b835d5855dc65fb81eb15c7a5d185256
Devi Acharya <dacharya64@gmail.com>
2021-11-11 10:32:16 -0800

    changing json

--- character.js
+++ character.js
@@ -1,9 +1,11 @@
-dragula([document.querySelector('#list3'), document.querySelector('#list'), document.querySelector('#list2')]);
+dragula([document.querySelector('#character-list'), document.querySelector('#problems-list'), document.querySelector('#edges-list')]);
 
 
 window.onload = function () {
 	//localStorage.clear();
+	$('#character-list').empty();
 	loadProblems();
+	loadEdges();
 }
 
 function loadProblems() {
@@ -22,14 +24,18 @@ function loadProblems() {
 }
 
 function parseProblems(results) {
-	var problemsList = document.getElementById("list");
+	var problemsList = document.getElementById("problems-list");
+	var characterList = document.getElementById("character-list");
   	// clear the current thing in the div
-	$('#list').empty();
+	$('#problems-list').empty();
 	// iterate through the JSON list of problems and display them
 	for (var i = 0; i < results.length; i++) {
+		var html = generateString(results[i], "problem-card");
+		//if the character has this problem, add it to the character list
 		if (results[i].obtained == false) {
-			var html = generateString(results[i]);
 			problemsList.insertAdjacentHTML( 'beforeend', html);
+		} else { 
+			characterList.insertAdjacentHTML( 'beforeend', html);
 		}
 	}
 }
@@ -50,19 +56,22 @@ function loadEdges() {
 }
 
 function parseEdges(results) {
-	var edgesList = document.getElementById("list2");
+	var edgesList = document.getElementById("edges-list");
+	var characterList = document.getElementById("character-list");
   	// clear the current thing in the div
-	$('#list2').empty();
+	$('#edges-list').empty();
 	// iterate through the JSON list of problems and display them
 	for (var i = 0; i < results.length; i++) {
+		var html = generateString(results[i], "edge-card");
 		if (results[i].obtained == false) {
-			var html = generateString(results[i]);
 			edgesList.insertAdjacentHTML( 'beforeend', html);
+		} else {
+			characterList.insertAdjacentHTML( 'beforeend', html);
 		}
 	}
 }
 
-function generateString(result) {
-	var string = "<div class='card'><h1>" + result.id + " </h1><p>" + result.name + " " + result.type + "</p><p>" + result.description + "</p></div>"
+function generateString(result, classType) {
+	var string = "<div class='" + classType + "'><h1>" + result.id + "  |  <strong>" + result.name + "</strong> " + result.type + "</h1><p>" + result.description + "</p></div>"
 	return string;
 }
